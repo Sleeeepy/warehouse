28b01d75bb7220a1924d467c7fb5b5cca9a0934c
Sleeeepy <elmarzimmermann@hotmail.com>
2015-04-30 18:10:50 +0100

    adding form

--- client/app/inventory/category/category.controller.js
+++ client/app/inventory/category/category.controller.js
@@ -4,6 +4,7 @@ angular.module('warehouseApp')
   .controller('CategoryCtrl', function ($scope,Inventory) {
     $scope.message = 'Hello';
     $scope.categories="";
+    $scope.category = {};
     var inventory = Inventory;
     inventory.category.query().$promise.then(function(categories){
 
@@ -15,5 +16,38 @@ angular.module('warehouseApp')
              $scope.categories=categories;
           });
     };
+
+
+    $scope.errors = {};
+
+    $scope.addCategory = function(form) {
+      console.log(form);
+      $scope.submitted = true;
+      return;
+
+
+      if(form.$valid) {
+        Auth.createUser({
+          name: $scope.user.name,
+          email: $scope.user.email,
+          password: $scope.user.password
+        })
+        .then( function() {
+          // Account created, redirect to home
+          $location.path('/');
+        })
+        .catch( function(err) {
+          err = err.data;
+          $scope.errors = {};
+
+          // Update validity of form fields that match the mongoose errors
+          angular.forEach(err.errors, function(error, field) {
+            form[field].$setValidity('mongoose', false);
+            $scope.errors[field] = error.message;
+          });
+        });
+      }
+    };
+
 
   });
