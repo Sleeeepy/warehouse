0d10717e1de37ba03ab0dc850e9f8fe1fe86d654
Sleeeepy <elmarzimmermann@hotmail.com>
2015-05-06 18:36:33 +0100

    category-update, image-save

--- client/app/inventory/category/category.controller.js
+++ client/app/inventory/category/category.controller.js
@@ -8,54 +8,65 @@ angular.module('warehouseApp')
     $scope.category = {};
     $scope.submitted = false;
     $scope.alert = {};
-
-
-
+    $scope.errors = {};
     var inventory = Inventory;
 
-    $scope.changeCat = function(cat){
+    $scope.uploadImages = function(flow){
+      var files = flow.files;
+      var fd = new FormData();
+
+      inventory.image.save({files:flow.files},function(result){
+        console.log(result);
+      });
+
+    };
+
+
+
+    $scope.selectCategory = function(cat){
 
       $scope.category=cat;
       var parent = $scope.categories.filter(function(obj){
-
         return obj._id === cat.parent;
       });
-      $scope.category.parent = parent[0];
-
-      console.log($scope.category);
-
+      if(parent[0]){$scope.category.parent = parent[0]._id;}
     }
 
 
-    $scope.updateCategories =  function(){
+    $scope.refreshCategories =  function(){
       inventory.category.query({level:1}).$promise.then(function(categories){
-             $scope.categories=categories;
-          });
+          $scope.categories=categories;
+      });
       inventory.category.tree().$promise.then(function(tree){
-                   $scope.tree=tree;
-                });
+          $scope.tree=tree;
+      });
     };
-    $scope.updateCategories();
-
-    $scope.errors = {};
+    $scope.refreshCategories();
 
     $scope.addCategory = function(form) {
       $scope.submitted = true;
       if(form.$valid) {
-        var newCat = new inventory.category($scope.category);
-        if (newCat.parent){newCat.parent = newCat.parent._id;}
+        if(!$scope.category._id){
+          var newCat = new inventory.category($scope.category);
+          newCat.$save().then(function(result){
+            $scope.alert={  type: 'success',
+                            msg: 'Category '+result.name+' added successfully.'
+                          };
+            });
+        }else{
+          inventory.category.update({id:$scope.category._id},$scope.category).$promise.then(function(result){
+            $scope.alert={  type: 'success',
+                            msg: 'Category '+result.name+' updated successfully.'
+                          };
+          });
+        }
 
-        newCat.$save().then(function(result){
-          $scope.alert={  type: 'success',
-                          msg: 'Category '+result.name+' added successfully.'
-                        };
-          $scope.updateCategories();
+          $scope.refreshCategories();
           setTimeout(function(){$scope.reset();},2500);
-        });
-
-        };
-
+      }
     };
+
+
     $scope.deleteCategory = function(category) {
       console.log('deleting',category.name);
 
@@ -65,7 +76,7 @@ angular.module('warehouseApp')
             $scope.updateCategories();
           },function(err){
             alert(err);
-            $scope.updateCategories();
+            $scope.refreshCategories();
           });
 
         });
@@ -86,126 +97,4 @@ angular.module('warehouseApp')
       $scope.$apply();
 
     };
-
-
-    /**
-     * @property interface
-     * @type {Object}
-     */
-    $scope.interface = {};
-
-    /**
-     * @property uploadCount
-     * @type {Number}
-     */
-    $scope.uploadCount = 0;
-
-    /**
-     * @property success
-     * @type {Boolean}
-     */
-    $scope.success = false;
-
-    /**
-     * @property error
-     * @type {Boolean}
-     */
-    $scope.error = false;
-
-    // Listen for when the interface has been configured.
-    $scope.$on('$dropletReady', function whenDropletReady() {
-
-        $scope.interface.allowedExtensions(['png', 'jpg', 'bmp', 'gif', 'svg', 'torrent']);
-        $scope.interface.setRequestUrl('upload.html');
-        $scope.interface.defineHTTPSuccess([/2.{2}/]);
-        $scope.interface.useArray(false);
-
-    });
-
-    // Listen for when the files have been successfully uploaded.
-    $scope.$on('$dropletSuccess', function onDropletSuccess(event, response, files) {
-
-        $scope.uploadCount = files.length;
-        $scope.success     = true;
-        console.log(response, files);
-
-        $timeout(function timeout() {
-            $scope.success = false;
-        }, 5000);
-
-    });
-
-    // Listen for when the files have failed to upload.
-    $scope.$on('$dropletError', function onDropletError(event, response) {
-
-        $scope.error = true;
-        console.log(response);
-
-        $timeout(function timeout() {
-            $scope.error = false;
-        }, 5000);
-
-    });
-
-}).directive('progressbar', function ProgressbarDirective() {
-
-    return {
-
-        /**
-         * @property restrict
-         * @type {String}
-         */
-        restrict: 'A',
-
-        /**
-         * @property scope
-         * @type {Object}
-         */
-        scope: {
-            model: '=ngModel'
-        },
-
-        /**
-         * @property ngModel
-         * @type {String}
-         */
-        require: 'ngModel',
-
-        /**
-         * @method link
-         * @param scope {Object}
-         * @param element {Object}
-         * @return {void}
-         */
-        link: function link(scope, element) {
-
-            var progressBar = new ProgressBar.Path(element[0], {
-                strokeWidth: 2
-            });
-
-            scope.$watch('model', function() {
-
-                progressBar.animate(scope.model / 100, {
-                    duration: 1000
-                });
-
-            });
-
-            scope.$on('$dropletSuccess', function onSuccess() {
-                progressBar.animate(0);
-            });
-
-            scope.$on('$dropletError', function onSuccess() {
-                progressBar.animate(0);
-            });
-
-        }
-
-    }
-
-
-
-
-
-
   });
--- client/app/inventory/inventory.service.js
+++ client/app/inventory/inventory.service.js
@@ -5,9 +5,37 @@ angular.module('warehouseApp')
     // Public API here
     return {
       category: $resource('/api/categories/:id',{id:'@_id'},{
-                    tree: {method:'GET',isArray:true, params:{tree:true}}
+                    tree: {method:'GET',isArray:true, params:{tree:true}},
+                    update:{ method:'PUT'}
                   }),
       product:  $resource('/api/products/:id'),
-      brand:    $resource('/api/brands/:id')
+      brand:    $resource('/api/brands/:id'),
+      image:    $resource('/api/images/:id',{id: "@_id"}, {
+                      save: { method: 'POST',
+                              transformRequest: function(data) {
+                                      if (data === undefined)
+                                        return data;
+
+                                      var fd = new FormData();
+                                      angular.forEach(data, function(value, key) {
+                                        if (value instanceof FileList) {
+                                          if (value.length == 1) {
+                                            fd.append(key, value[0]);
+                                          } else {
+                                            angular.forEach(value, function(file, index) {
+                                              fd.append(key + '_' + index, file);
+                                            });
+                                          }
+                                        } else {
+                                          fd.append(key, value);
+                                        }
+                                      });
+
+                                      return fd;
+                                    }
+                              //header: <SEE BELOW>
+                              // todo https://github.com/angular/angular.js/issues/1375#issuecomment-21933012
+                      }
+                    })
     };
   });
